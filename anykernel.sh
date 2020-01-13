55486db2518a05d79aacc9e78e29d1525a880126
HarukeyUA <inazarrusnak@gmail.com>
2020-01-13 15:13:07 +0200

    ak3: patch to remove magisk hack
    Change-Id: 620bd3d73ab5e6d7d5befd1e764755c07f3e29e6
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- anykernel.sh
+++ anykernel.sh
@@ -39,6 +39,9 @@ patch_vbmeta_flag=auto;
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
+# remove old root patch avoidance hack
+patch_cmdline "skip_override" "";
+
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
 
