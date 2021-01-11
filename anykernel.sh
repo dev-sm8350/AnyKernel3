4bdeb396c775cb9a55c54aa7302db3380273968e
Danny Lin <danny@kdrag0n.dev>
2021-01-10 18:00:43 -0800

    anykernel: Auto-detect boot block device
    Change-Id: 619850429711e3bf96953037d2acd519345a4dc2
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- anykernel.sh
+++ anykernel.sh
@@ -31,7 +31,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/bootdevice/by-name/boot;
+BLOCK=auto;
 IS_SLOT_DEVICE=auto;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
