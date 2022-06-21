b286cd6f0a9d9f7818572a486dc62228a1739af1
engstk <eng.stk@sapo.pt>
2022-06-21 19:54:56 +0100

    ak3: op9-ify
    * To Custom Rom
    
    Change-Id: 776799b78d051fe316f40ab12bb3436af2a4fc10
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- anykernel.sh
+++ anykernel.sh
@@ -13,12 +13,24 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=OnePlus9Pro
+device.name2=lemonadep
+device.name3=OnePlus9ProTMO
+device.name4=lemonadept
+device.name5=OnePlus9
+device.name6=lemonade
+device.name7=OnePlus9TMO
+device.name8=lemonadet
+device.name9=OnePlus9VZW
+device.name10=lemonadev
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
+### AnyKernel install
+## trim partitions
+fstrim -v /data;
 
 ### AnyKernel install
 ## boot files attributes
@@ -28,7 +40,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=;
+block=boot;
 is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
@@ -91,17 +103,18 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 #} # end attributes
 
 # vendor_boot shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+block=vendor_boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
 
 # reset for vendor_boot patching
-#reset_ak;
+reset_ak;
 
 # vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
 
