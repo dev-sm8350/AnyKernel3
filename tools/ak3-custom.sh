0f48ec17c534a92f3e96bd484e8171eaf5d04edc
Alexander Winkowski <dereference23@outlook.com>
2024-09-01 18:25:21 +0000

    AK3: Erase dtbo
    [dereference23]:
    I have reversed dtbo and inlined it into dtb. Starting from this e+ version, dtbo partition is not used any more.
    
    To users: just flash new zip and check for any bugs
    To maintainers: you can remove dtbo flags from device tree once the new source is out
    To anyone who is interested: you can verify if dtbo is really unused by erasing this partition
    dd if=/dev/zero of=/dev/block/by-name/dtbo$(getprop ro.boot.slot_suffix) conv=fsync bs=24M
    
    Change-Id: 81a8c85671ad615f96a01398e816cef39416075c
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- tools/ak3-custom.sh
+++ tools/ak3-custom.sh
@@ -7,3 +7,7 @@ check_cmdline() {
     echo cmdline=$(tr '\n' ' ' < $AKHOME/cmdline) >> $SPLITIMG/header;
   fi;
 }
+
+erase_dtbo() {
+  dd if=/dev/zero of=/dev/block/by-name/dtbo$SLOT conv=fsync count=1 bs=$(blockdev --getsize64 /dev/block/by-name/dtbo$SLOT);
+}
