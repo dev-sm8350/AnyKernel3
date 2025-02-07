7d4e34b42bc609def25de778f6e1cf3879249f5f
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-02-07 10:20:36 +0530

    anykernel: Change kernel string to Nebula

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=e+ kernel by dereference
+kernel.string=Nebula kernel by Frost
 do.devicecheck=1
 do.cleanup=1
 device.name1=veux
