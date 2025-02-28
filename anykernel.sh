d6b50b08b23518ff63789ec85eaf7a13bb45140e
TogoFire <togofire@mailfence.com>
2025-02-28 10:05:16 -0300

    ak3: veux-ify
    * To Custom Rom
    
    Change-Id: a9d67d933f115aa37c360eac063ead762857f27c
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- anykernel.sh
+++ anykernel.sh
@@ -13,12 +13,16 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=veux
+device.name2=peux
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
+### AnyKernel install
+## trim partitions
+fstrim -v /data;
 
 ### AnyKernel install
 ## boot files attributes
@@ -28,7 +32,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=;
+BLOCK=boot;
 IS_SLOT_DEVICE=auto;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
@@ -91,17 +95,17 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 #} # end attributes
 
 # vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+BLOCK=vendor_boot;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 # reset for vendor_boot patching
-#reset_ak;
+reset_ak;
 
 # vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
 
