53791e1baf9b2f5e294aa18f8dddc97431aafe8b
TogoFire <togofire@mailfence.com>
2025-02-28 10:15:10 -0300

    Backend: Nebula-ify
    Change-Id: 6b0bdf255947bf36fece247a864657a979934a48
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,10 +4,10 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=
-kernel.compiler=
-kernel.made=
-message.word=
+kernel.string=Nebula
+kernel.compiler=WeebX Clang
+kernel.made=Frost
+message.word=Join @frosty_builds & @TogoFireWork
 do.devicecheck=1
 do.modules=0
 do.systemless=1
